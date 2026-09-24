e7089bf39311325cb51e920132541bd4ce1b5ce3
Akshay-Kumar-06 <akshay01829@gmail.com>
2026-09-24 12:54:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>CI/CD Demo 🚀</h1>
+      <p>Hello, Akshay!</p>
+      <p>Version 2.0</p>
+    </div>
+  );
+}
+
+
+
+export default App;
